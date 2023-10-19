8b8915beb791bf1b59c54d6084bb851b0e5814be
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2023-10-19 13:25:14 +0200

    feat: boolean extensions
    changes:
    - extensions methods added
    - unit tests added
    
    closes #4

---
+++ src/BB84.Extensions/BooleanExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace BB84.Extensions;
+
+/// <summary>
+/// The boolean extensions class.
+/// </summary>
+public static class BooleanExtensions
+{
+	/// <summary>
+	/// Checks if the provided value is true.
+	/// </summary>
+	/// <param name="value">The <see cref="bool"/> value to work with.</param>
+	/// <returns><see langword="true"/> or <see langword="false"/></returns>
+	public static bool IsTrue(this bool value)
+		=> value.Equals(true);
+
+	/// <summary>
+	/// Checks if the provided value is false.
+	/// </summary>
+	/// <param name="boolValue">The <see cref="bool"/> value to work with.</param>
+	/// <returns><see langword="true"/> or <see langword="false"/></returns>
+	public static bool IsFalse(this bool boolValue)
+		=> boolValue.Equals(false);
+}
---
+++ tests/BB84.ExtensionsTests/BooleanExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using BB84.Extensions;
+
+namespace BB84.ExtensionsTests;
+
+[TestClass]
+public class BooleanExtensionsTests
+{
+	[TestMethod]
+	public void IsTrueTest()
+	{
+		bool value = true;
+
+		bool result = value.IsTrue();
+
+		Assert.IsTrue(result);
+	}
+
+	[TestMethod]
+	public void IsFalseTest()
+	{
+		bool value = false;
+
+		bool result = value.IsFalse();
+
+		Assert.IsTrue(result);
+	}
+}
