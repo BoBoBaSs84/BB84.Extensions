faafdd82997f1c0e46c81a55adc013b82a08060c
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2023-10-20 16:40:08 +0200

    feat: integer extensions
    changes:
    - `ToInt32Invariant` method added
    - unit tests added
    
    closes #8

---
+++ src/BB84.Extensions/IntegerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace BB84.Extensions;
+
+/// <summary>
+/// The integer extensions class.
+/// </summary>
+public static class IntegerExtensions
+{
+	/// <summary>
+	/// Converts the value of the specified object to a 32-bit signed integer, using invariant culture formatting information.
+	/// </summary>
+	/// <param name="value">An object that implements the <see cref="IConvertible"/> interface.</param>
+	/// <returns>A 32-bit signed integer that is equivalent to value, or zero if value is null.</returns>
+	public static int ToInt32Invariant(this object? value)
+		=> Convert.ToInt32(value, CultureInfo.InvariantCulture);
+}
---
+++ tests/BB84.ExtensionsTests/IntegerExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using BB84.Extensions;
+
+namespace BB84.ExtensionsTests;
+
+[TestClass]
+public class IntegerExtensionsTests
+{
+	[TestMethod]
+	public void ToInt32InvariantTest()
+	{
+		object obj = 5;
+
+		int i = obj.ToInt32Invariant();
+
+		Assert.AreEqual(5, i);
+	}
+}
