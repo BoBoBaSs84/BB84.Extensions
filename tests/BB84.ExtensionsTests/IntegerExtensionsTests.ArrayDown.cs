138a6452168db6ffa234bfbddefcb0d8b97ea439
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2023-10-22 09:18:17 +0200

    feat: integer extensions
    changes:
    - the `For` method added
    - the `ArrayUp` method added
    - the `ArrayDown` method added
    - unit tests added
    
    closes #24

---
+++ tests/BB84.ExtensionsTests/IntegerExtensionsTests.ArrayDown.cs
@@ -0,0 +1,28 @@
+﻿using BB84.Extensions;
+
+namespace BB84.ExtensionsTests;
+
+public sealed partial class IntegerExtensionsTests
+{
+	[TestMethod]
+	[Description("Should create an array of integers to a given maximum.")]
+	public void ArrayDown()
+	{
+		int value = 15;
+		int minValue = 0;
+
+		int[] array = value.ArrayDown(minValue);
+
+		Assert.AreEqual(16, array.Length);
+	}
+
+	[TestMethod]
+	[Description("Should throw an exception.")]
+	[ExpectedException(typeof(ArgumentOutOfRangeException))]
+	public void ArrayDownException()
+	{
+		int value = 0;
+		int minValue = 15;
+		_ = value.ArrayDown(minValue);
+	}
+}
---
+++ tests/BB84.ExtensionsTests/IntegerExtensionsTests.ArrayUp.cs
@@ -0,0 +1,28 @@
+﻿using BB84.Extensions;
+
+namespace BB84.ExtensionsTests;
+
+public sealed partial class IntegerExtensionsTests
+{
+	[TestMethod]
+	[Description("Should create an array of integers to a given maximum.")]
+	public void ArrayUp()
+	{
+		int value = 0;
+		int maxValue = 15;
+
+		int[] array = value.ArrayUp(maxValue);
+
+		Assert.AreEqual(16, array.Length);
+	}
+
+	[TestMethod]
+	[Description("Should throw an exception.")]
+	[ExpectedException(typeof(ArgumentOutOfRangeException))]
+	public void ArrayUpException()
+	{
+		int value = 15;
+		int maxValue = 0;
+		_ = value.ArrayUp(maxValue);
+	}
+}
