3e1e035dc4d0c3072fe58adc9682cedad9ed75e7
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2023-10-30 13:43:00 +0100

    fix: integer extension method for
    changes:
    - method fixed
    - unit test fixed
    
    closes #48

--- src/BB84.Extensions/IntegerExtensions.For.cs
+++ src/BB84.Extensions/IntegerExtensions.For.cs
@@ -5,12 +5,11 @@ public static partial class IntegerExtensions
 	/// <summary>
 	/// Iterates over an integer array and executes an Action on each element.
 	/// </summary>
-	/// <param name="value">The value from which to iterate.</param>
-	/// <param name="maxValue">The value up to which to iterate.</param>
+	/// <param name="value">The value until which to iterate.</param>
 	/// <param name="action">An Action to invoke on each integer.</param>
-	public static void For(this int value, int maxValue, Action<int> action)
+	public static void For(this int value, Action<int> action)
 	{
-		for (int i = value; i <= maxValue; i++)
+		for (int i = 0; i < value; i++)
 			action(i);
 	}
 }
