c2b882bb2cbfe672174771fc30a3d69a335bf3a6
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-03-08 20:36:05 +0100

    feat: stream extensions
    changes:
    - the `ToByteArray` method added
    - unit tests added
    - version bump to `2.2.0`
    
    closes #88

---
+++ src/BB84.Extensions/StreamExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace BB84.Extensions;
+
+/// <summary>
+/// The stream extensions class.
+/// </summary>
+public static partial class StreamExtensions
+{
+	/// <summary>
+	/// The method will keep reading (and copying into a <see cref="MemoryStream"/>)
+	/// until it runs out of data.
+	/// </summary>
+	/// <param name="inputStream">The stream to work with.</param>
+	/// <returns>The <see cref="Stream"/> as <see cref="byte"/> array.</returns>
+	public static byte[] ToByteArray(this Stream inputStream)
+	{
+		byte[] buffer = new byte[16 * 1024];
+		using MemoryStream memoryStream = new();
+		int read;
+		while ((read = inputStream.Read(buffer, 0, buffer.Length)) > 0)
+			memoryStream.Write(buffer, 0, read);
+		return memoryStream.ToArray();
+	}
+}
---
+++ tests/BB84.ExtensionsTests/StreamExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using BB84.Extensions;
+
+namespace BB84.ExtensionsTests;
+
+[TestClass]
+public sealed partial class StreamExtensionsTests
+{
+	private readonly static Random Random = new();
+
+	[DataTestMethod]
+	[DynamicData(nameof(GetData), DynamicDataSourceType.Method)]
+	public void ToByteArrayTest(byte[] buffer)
+	{
+		MemoryStream stream = new(buffer);
+
+		byte[] result = stream.ToByteArray();
+
+		Assert.IsTrue(result.SequenceEqual(buffer));
+	}
+
+	private static IEnumerable<object[]> GetData()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			byte[] buffer = new byte[10];
+			Random.NextBytes(buffer);
+			yield return new object[] { buffer };
+		}
+	}
+}
