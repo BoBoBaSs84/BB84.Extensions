8feed01c82a3ef8c28ed5e3dd8133a8602dafe10
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-07-17 08:27:46 +0200

    fix: descriptions fixed

--- src/BB84.Extensions/ColorExtensions.ToARGBHexString.cs
+++ src/BB84.Extensions/ColorExtensions.ToARGBHexString.cs
@@ -5,7 +5,7 @@ namespace BB84.Extensions;
 public static partial class ColorExtensions
 {
 	/// <summary>
-	/// Retunrs the <see cref="Color"/> as ARGB hex representation (i.e. <b>#FF00FF00</b>)
+	/// Returns the <see cref="Color"/> as ARGB hex representation (i.e. <b>#FF00FF00</b>)
 	/// </summary>
 	/// <param name="value">The color to convert.</param>
 	/// <returns>The color as hex string.</returns>
