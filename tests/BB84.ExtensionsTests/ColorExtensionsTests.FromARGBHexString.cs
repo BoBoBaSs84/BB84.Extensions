892bfd1bfa2a799a7a9a84a521e9325fb7c86185
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-07-17 08:41:30 +0200

    feat: unit tests added

---
+++ tests/BB84.ExtensionsTests/ColorExtensionsTests.FromARGBHexString.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+using BB84.Extensions;
+
+namespace BB84.ExtensionsTests;
+
+public sealed partial class ColorExtensionsTests
+{
+	[DataTestMethod]
+	[DynamicData(nameof(GetArgbTestData), DynamicDataSourceType.Method)]
+	public void FromARGBHexStringTest(Color expected, string value)
+		=> Assert.AreEqual(expected.ToArgb(), value.FromARGBHexString().ToArgb());
+}
---
+++ tests/BB84.ExtensionsTests/ColorExtensionsTests.FromRGBHexString.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+using BB84.Extensions;
+
+namespace BB84.ExtensionsTests;
+
+public sealed partial class ColorExtensionsTests
+{
+	[DataTestMethod]
+	[DynamicData(nameof(GetRgbTestData), DynamicDataSourceType.Method)]
+	public void FromRGBHexStringTest(Color expected, string value)
+		=> Assert.AreEqual(expected.ToArgb(), value.FromRGBHexString().ToArgb());
+}
