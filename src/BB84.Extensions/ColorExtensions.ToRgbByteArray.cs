f60ed86561860d90f38eb39bda757f8be1de0ac1
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-07-27 13:03:08 +0200

    feat: four extension methods added

---
+++ src/BB84.Extensions/ColorExtensions.ToRgbByteArray.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace BB84.Extensions;
+
+public static partial class ColorExtensions
+{
+	/// <summary>
+	/// Converts to provided <paramref name="color"/> into a three byte array.
+	/// </summary>
+	/// <param name="color">The color to convert.</param>
+	/// <returns>The three byte array.</returns>
+	public static byte[] ToRgbByteArray(this Color color)
+	{
+		byte[] bytes = [color.B, color.G, color.R];
+
+		if (!BitConverter.IsLittleEndian)
+			Array.Reverse(bytes);
+
+		return bytes;
+	}
+}
