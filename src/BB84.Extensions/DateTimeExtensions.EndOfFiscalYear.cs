bc5198e20c26adf540c664845ca9e74b9c6e1b87
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-07-27 13:30:14 +0200

    feat: four new extension methods added

---
+++ src/BB84.Extensions/DateTimeExtensions.EndOfFiscalYear.cs
@@ -0,0 +1,22 @@
+﻿namespace BB84.Extensions;
+
+public static partial class DateTimeExtensions
+{
+	/// <summary>
+	/// Returns the end date of the fiscal year using the specified <paramref name="value"/>
+	/// and the definition of the <paramref name="startMonth"/> of the fiscal year.
+	/// </summary>
+	/// <param name="value">The date time value to work with.</param>
+	/// <param name="startMonth">The first month of the fiscal year.</param>
+	/// <returns>The end date of the fiscal year.</returns>
+	/// <exception cref="ArgumentOutOfRangeException"></exception>
+	public static DateTime EndOfFiscalYear(this DateTime value, int startMonth = 10)
+	{
+		if (startMonth is < 1 or > 12)
+			throw new ArgumentOutOfRangeException(nameof(startMonth), "Must be between 1 and 12.");
+
+		DateTime endOfFiscalYear = StartOfFiscalYear(value, startMonth).AddYears(1).AddDays(-1);
+
+		return endOfFiscalYear;
+	}
+}
---
+++ src/BB84.Extensions/DateTimeExtensions.StartOfYear.cs
@@ -0,0 +1,12 @@
+﻿namespace BB84.Extensions;
+
+public static partial class DateTimeExtensions
+{
+	/// <summary>
+	/// Returns the first day of the year using the specified date.
+	/// </summary>
+	/// <param name="dateTime">The date time value to work with.</param>
+	/// <returns>The date of the first day of the year.</returns>
+	public static DateTime StartOfYear(this DateTime dateTime)
+		=> new(dateTime.Year, 1, 1);
+}
