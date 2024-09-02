ea7e185c1b2f8b0c9d6dd620e93560d909d55a0a
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-02 11:05:50 +0200

    fix: diagnostics for regex source generation

--- src/BB84.Extensions/StringExtensions.cs
+++ src/BB84.Extensions/StringExtensions.cs
@@ -7,7 +7,22 @@ namespace BB84.Extensions;
 /// </summary>
 public static partial class StringExtensions
 {
+#if NET7_0_OR_GREATER
+	private static readonly Regex Base64Regex = GeneratedBase64Regex();
+	private static readonly Regex WhitespaceRegex = GeneratedWhitespaceRegex();
+	private static readonly Regex LinebreakRegex = GeneratedLinebreakRegex();
+
+	[GeneratedRegex(@"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None)]
+	private static partial Regex GeneratedBase64Regex();
+
+	[GeneratedRegex(@"\s+", RegexOptions.None)]
+	private static partial Regex GeneratedWhitespaceRegex();
+
+	[GeneratedRegex(@"(\r\n|\r|\n)", RegexOptions.None)]
+	private static partial Regex GeneratedLinebreakRegex();
+#else
 	private static readonly Regex Base64Regex = new(@"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None);
 	private static readonly Regex WhitespaceRegex = new(@"\s+", RegexOptions.None);
 	private static readonly Regex LinebreakRegex = new(@"(\r\n|\r|\n)", RegexOptions.None);
+#endif
 }
