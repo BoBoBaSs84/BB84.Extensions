1110f29701002ed2a459064e1878f78f23898017
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-23 17:40:19 +0200

    feat: unit tests for nullable bool methods added

--- tests/BB84.ExtensionsTests/BooleanExtensionsTests.IsFalse.cs
+++ tests/BB84.ExtensionsTests/BooleanExtensionsTests.IsFalse.cs
@@ -9,4 +9,11 @@ public partial class BooleanExtensionsTests
 	[DataRow(false, true)]
 	public void IsFalseTest(bool value, bool expected)
 		=> Assert.AreEqual(expected, value.IsFalse());
+
+	[DataTestMethod]
+	[DataRow(true, false)]
+	[DataRow(false, true)]
+	[DataRow(null, false)]
+	public void NullableIsFalseTest(bool? value, bool expected)
+		=> Assert.AreEqual(expected, value.IsFalse());
 }
