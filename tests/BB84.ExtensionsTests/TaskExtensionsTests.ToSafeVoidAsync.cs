6c594924e493042866d6807ec7da2ed0d9e36b20
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-23 19:32:57 +0200

    feat: task extension unit tests added

---
+++ tests/BB84.ExtensionsTests/TaskExtensionsTests.ToSafeVoidAsync.cs
@@ -0,0 +1,32 @@
+﻿using BB84.Extensions;
+
+namespace BB84.ExtensionsTests;
+
+public sealed partial class TaskExtensionsTests
+{
+	[TestMethod]
+	public void ToAsyncVoidTest()
+	{
+		bool taskCompleted = false;
+		bool exceptionOccured = false;
+
+		AwaitableTask().ToAsyncVoid(() => taskCompleted = true, (e) => exceptionOccured = true);
+
+		Assert.IsFalse(exceptionOccured);
+		Assert.IsTrue(taskCompleted);
+	}
+
+	[TestMethod]
+	public void ToAsyncVoidExceptionTest()
+	{
+		bool taskCompleted = false;
+		bool exceptionOccured = false;
+
+		AwaitableTask(true).ToAsyncVoid(() => taskCompleted = true, (e) => exceptionOccured = true);
+
+		Task.Delay(100).Wait();
+
+		Assert.IsTrue(exceptionOccured);
+		Assert.IsTrue(taskCompleted);
+	}
+}
---
+++ tests/BB84.ExtensionsTests/TaskExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿namespace BB84.ExtensionsTests;
+
+[TestClass]
+public sealed partial class TaskExtensionsTests
+{
+	private static async Task<bool> BoolTask(bool throwException = false)
+	{
+		if (throwException)
+		{
+			await Task.Delay(50)
+				.ConfigureAwait(true);
+
+			throw new InvalidOperationException();
+		}
+
+		return true;
+	}
+
+	private static async Task AwaitableTask(bool throwException = false)
+	{
+		if (throwException)
+		{
+			await Task.Delay(50)
+				.ConfigureAwait(true);
+
+			throw new InvalidOperationException();
+		}
+	}
+}
