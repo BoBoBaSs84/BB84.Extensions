746235150ed736f5204f2d9c33b23fbe044af5c6
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-23 19:32:57 +0200

    feat: task extension unit tests added

---
+++ tests/BB84.ExtensionsTests/TaskExtensionsTests.ToSafeVoid.cs
@@ -0,0 +1,30 @@
+﻿using BB84.Extensions;
+
+namespace BB84.ExtensionsTests;
+
+public sealed partial class TaskExtensionsTests
+{
+	[TestMethod]
+	public void ToSyncVoidTest()
+	{
+		bool taskCompleted = false;
+		bool exceptionOccured = false;
+
+		AwaitableTask().ToSyncVoid(() => taskCompleted = true, (e) => exceptionOccured = true);
+
+		Assert.IsFalse(exceptionOccured);
+		Assert.IsTrue(taskCompleted);
+	}
+
+	[TestMethod]
+	public void ToSyncVoidExceptionTest()
+	{
+		bool taskCompleted = false;
+		bool exceptionOccured = false;
+
+		AwaitableTask(true).ToSyncVoid(() => taskCompleted = true, (e) => exceptionOccured = true);
+
+		Assert.IsTrue(exceptionOccured);
+		Assert.IsTrue(taskCompleted);
+	}
+}
