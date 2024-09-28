5abb866fcb0b0761018b418df3db515d41ca159a
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-28 16:11:01 +0200

    fix: nullable string fix applied

--- src/BB84.Extensions/StringExtensions.IsNull.cs
+++ src/BB84.Extensions/StringExtensions.IsNull.cs
@@ -1,4 +1,6 @@
-﻿namespace BB84.Extensions;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace BB84.Extensions;
 
 public static partial class StringExtensions
 {
@@ -8,7 +10,7 @@ public static partial class StringExtensions
 	/// <param name="value">The string value to test.</param>
 	/// <returns><see langword="true"/> if the <paramref name="value"/>
 	/// is <see langword="null"/>, otherwise <see langword="false"/>.</returns>
-	public static bool IsNull(this string value)
+	public static bool IsNull(this string? value)
 		=> value is null;
 
 	/// <summary>
@@ -16,7 +18,7 @@ public static partial class StringExtensions
 	/// </summary>
 	/// <param name="value">The string value to test.</param>
 	/// <returns>True if the value parameter is null or an empty string, otherwise false.</returns>
-	public static bool IsNullOrEmpty(this string value)
+	public static bool IsNullOrEmpty(this string? value)
 		=> string.IsNullOrEmpty(value);
 
 	/// <summary>
@@ -26,6 +28,6 @@ public static partial class StringExtensions
 	/// <param name="value">The string value to test.</param>
 	/// <returns>True if the value parameter is null or an empty string or if value consists
 	/// exclusively of white-space characters, otherwise false.</returns>
-	public static bool IsNullOrWhiteSpace(this string value)
+	public static bool IsNullOrWhiteSpace(this string? value)
 		=> string.IsNullOrWhiteSpace(value);
 }
