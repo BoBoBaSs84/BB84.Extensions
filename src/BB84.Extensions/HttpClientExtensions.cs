41b4177f47eeecea4db5708d349d09043a9e1cfc
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-29 21:52:16 +0200

    feat: added extension methods

---
+++ src/BB84.Extensions/HttpClientExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Headers;
+
+namespace BB84.Extensions;
+
+/// <summary>
+/// The <see cref="HttpClient"/> extensions class.
+/// </summary>
+public static class HttpClientExtensions
+{
+	private const string BearerScheme = "Bearer";
+
+	/// <summary>
+	/// Adds the specified bearer <paramref name="token"/> to the http client request header.
+	/// </summary>
+	/// <param name="client">The http client which should use the token.</param>
+	/// <param name="token">The bearer token to be used.</param>
+	/// <returns>The enriched http client.</returns>
+	public static HttpClient AddBearerToken(this HttpClient client, string token)
+	{
+		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(BearerScheme, token);
+
+		return client;
+	}
+
+	/// <summary>
+	/// Adds the specified <paramref name="mediaType"/> the Accept header for an HTTP request.
+	/// </summary>
+	/// <param name="client">The http client which should use the media type.</param>
+	/// <param name="mediaType">The media type to be used.</param>
+	/// <returns></returns>
+	public static HttpClient AddMediaType(this HttpClient client, string mediaType)
+	{
+		client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(mediaType));
+
+		return client;
+	}
+}
