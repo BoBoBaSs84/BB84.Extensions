3c035dd4408ce578870b659278d6ae3d5c10cba6
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-30 07:45:04 +0200

    feat: byte array to base64 extension methods added

--- src/BB84.Extensions/StringExtensions.FromBase64.cs
+++ src/BB84.Extensions/StringExtensions.FromBase64.cs
@@ -7,24 +7,13 @@ public static partial class StringExtensions
 	/// <summary>
 	/// Decodes the provided string from a base64 string.
 	/// </summary>
-	/// <param name="stringValue">The input string to work with.</param>
+	/// <param name="value">The input string to work with.</param>
 	/// <param name="encoding">The encoding to use.</param>
 	/// <returns>The decoded string.</returns>
 	/// <exception cref="ArgumentException">The provided string is not a valid base64 string.</exception>
-	public static string FromBase64(this string stringValue, Encoding? encoding = null)
+	public static string FromBase64(this string value, Encoding encoding)
 	{
-		if (stringValue.IsNullOrEmpty())
-			return string.Empty;
-
-		// Check for valid base64
-		stringValue = stringValue.Trim();
-		bool isValidBase64 = (stringValue.Length % 4 == 0) && Base64Regex.IsMatch(stringValue);
-
-		if (isValidBase64.Equals(false))
-			throw new ArgumentException($"{stringValue} is not valid base64");
-
-		byte[] buffer = Convert.FromBase64String(stringValue);
-		encoding ??= Encoding.Default;
+		byte[] buffer = value.FromBase64();
 		return encoding.GetString(buffer);
 	}
 }
