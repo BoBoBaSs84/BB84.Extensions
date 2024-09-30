6e250f04388f647cfea41d52aaf671562e9815c6
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-30 07:45:29 +0200

    feat: extension methods unit tests added

---
+++ tests/BB84.ExtensionsTests/ByteExtensionsTests.FromBase64.cs
@@ -0,0 +1,12 @@
+﻿using BB84.Extensions;
+
+namespace BB84.ExtensionsTests;
+
+public sealed partial class ByteExtensionsTests
+{
+	[DataTestMethod]
+	[DataRow(new byte[] { 85, 110, 105, 116, 84, 101, 115, 116 }, "VW5pdFRlc3Q=")]
+	[DataRow(new byte[0], "")]
+	public void FromBase64Test(byte[] expected, string value)
+		=> Assert.IsTrue(expected.SequenceEqual(value.FromBase64()));
+}
