b6a3b12321a3cc2efb5d5d596f1ea1038c5b8403
Probotus <137200367+Probotus@users.noreply.github.com>
2025-03-22 17:24:45 +0100

    feat: `PictureBox` control extensions added
    changes:
    - extension methods added
    - unit tests added
    
    closes #167

---
+++ src/BB84.WinForms.Extensions/PictureBoxExtensions.cs
@@ -0,0 +1,59 @@
+﻿namespace BB84.WinForms.Extensions;
+
+/// <summary>
+/// The extension methods for the <see cref="PictureBox"/> control.
+/// </summary>
+public static class PictureBoxExtensions
+{
+	/// <summary>
+	/// Binds the <see cref="Control.Enabled"/> property to the specified data source.
+	/// </summary>
+	/// <param name="pictureBox">The <see cref="PictureBox"/> control to bind.</param>
+	/// <param name="dataSource">The data source to bind to.</param>
+	/// <param name="dataMember">The property of the data source to bind to.</param>
+	/// <returns>The same <see cref="PictureBox"/> control instance, so that multiple calls can be chained.</returns>
+	public static PictureBox WithEnabledBinding(this PictureBox pictureBox, object dataSource, string dataMember)
+	{
+		pictureBox.DataBindings.Add(nameof(pictureBox.Enabled), dataSource, dataMember, true, DataSourceUpdateMode.OnPropertyChanged);
+		return pictureBox;
+	}
+
+	/// <summary>
+	/// Binds the <see cref="PictureBox.ErrorImage"/> property to the specified data source.
+	/// </summary>
+	/// <param name="pictureBox">The <see cref="PictureBox"/> control to bind.</param>
+	/// <param name="dataSource">The data source to bind to.</param>
+	/// <param name="dataMember">The property of the data source to bind to.</param>
+	/// <returns>The same <see cref="PictureBox"/> control instance, so that multiple calls can be chained.</returns>
+	public static PictureBox WithErrorImageBinding(this PictureBox pictureBox, object dataSource, string dataMember)
+	{
+		pictureBox.DataBindings.Add(nameof(pictureBox.ErrorImage), dataSource, dataMember, true, DataSourceUpdateMode.OnPropertyChanged);
+		return pictureBox;
+	}
+
+	/// <summary>
+	/// Binds the <see cref="PictureBox.Image"/> property to the specified data source.
+	/// </summary>
+	/// <param name="pictureBox">The <see cref="PictureBox"/> control to bind.</param>
+	/// <param name="dataSource">The data source to bind to.</param>
+	/// <param name="dataMember">The property of the data source to bind to.</param>
+	/// <returns>The same <see cref="PictureBox"/> control instance, so that multiple calls can be chained.</returns>
+	public static PictureBox WithImageBinding(this PictureBox pictureBox, object dataSource, string dataMember)
+	{
+		pictureBox.DataBindings.Add(nameof(pictureBox.Image), dataSource, dataMember, true, DataSourceUpdateMode.OnPropertyChanged);
+		return pictureBox;
+	}
+
+	/// <summary>
+	/// Binds the <see cref="Control.Visible"/> property to the specified data source.
+	/// </summary>
+	/// <param name="pictureBox">The <see cref="PictureBox"/> control to bind.</param>
+	/// <param name="dataSource">The data source to bind to.</param>
+	/// <param name="dataMember">The property of the data source to bind to.</param>
+	/// <returns>The same <see cref="PictureBox"/> control instance, so that multiple calls can be chained.</returns>
+	public static PictureBox WithVisibleBinding(this PictureBox pictureBox, object dataSource, string dataMember)
+	{
+		pictureBox.DataBindings.Add(nameof(pictureBox.Visible), dataSource, dataMember, true, DataSourceUpdateMode.OnPropertyChanged);
+		return pictureBox;
+	}
+}
---
+++ tests/BB84.WinForms.Extensions.Tests/PictureBoxExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿namespace BB84.WinForms.Extensions.Tests;
+
+[TestClass]
+public sealed class PictureBoxExtensionsTests
+{
+	[TestMethod]
+	public void WithEnabledBindingTest()
+	{
+		PictureBox pictureBox = new();
+
+		pictureBox.WithEnabledBinding(new object(), nameof(pictureBox.Enabled));
+
+		Assert.AreEqual(1, pictureBox.DataBindings.Count);
+		Assert.AreEqual(nameof(pictureBox.Enabled), pictureBox.DataBindings[0].PropertyName);
+	}
+
+	[TestMethod]
+	public void WithErrorImageBindingTest()
+	{
+		PictureBox pictureBox = new();
+
+		pictureBox.WithErrorImageBinding(new object(), nameof(pictureBox.ErrorImage));
+
+		Assert.AreEqual(1, pictureBox.DataBindings.Count);
+		Assert.AreEqual(nameof(pictureBox.ErrorImage), pictureBox.DataBindings[0].PropertyName);
+	}
+
+	[TestMethod]
+	public void WithImageBindingTest()
+	{
+		PictureBox pictureBox = new();
+
+		pictureBox.WithImageBinding(new object(), nameof(pictureBox.Image));
+
+		Assert.AreEqual(1, pictureBox.DataBindings.Count);
+		Assert.AreEqual(nameof(pictureBox.Image), pictureBox.DataBindings[0].PropertyName);
+	}
+
+	[TestMethod]
+	public void WithVisibleBindingTest()
+	{
+		PictureBox pictureBox = new();
+
+		pictureBox.WithVisibleBinding(new object(), nameof(pictureBox.Visible));
+
+		Assert.AreEqual(1, pictureBox.DataBindings.Count);
+		Assert.AreEqual(nameof(pictureBox.Visible), pictureBox.DataBindings[0].PropertyName);
+	}
+}
