da728cb97ee9c57fd7486091c2e385a15096a589
Probotus <137200367+Probotus@users.noreply.github.com>
2025-07-10 12:43:09 +0200

    feature: double is not null extension method and unit test added

---
+++ tests/BB84.Extensions.Tests/DoubleExtensionsTests.IsNotNull.cs
@@ -0,0 +1,15 @@
+﻿// Copyright: 2023 Robert Peter Meyer
+// License: MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+namespace BB84.Extensions.Tests;
+
+public sealed partial class DoubleExtensionsTests
+{
+	[DataTestMethod]
+	[DataRow(1.67d, true)]
+	[DataRow(null, false)]
+	public void IsNotNullTest(double? value, bool expected)
+	=> Assert.AreEqual(expected, value.IsNotNull());
+}
