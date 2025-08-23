3aaa5c6fb0a9982128b9e67f7d4f1d934bc3361a
Probotus <137200367+Probotus@users.noreply.github.com>
2025-08-23 21:36:36 +0200

    feat: enumeration binding added
    changes:
    - added new `WithDisplayMember` method
    - added new `WithValueMember` method
    - added new `WithEnumeratorBinding` method
    - added unit tests

--- src/BB84.WinForms.Extensions/ListControlExtensions.cs
+++ src/BB84.WinForms.Extensions/ListControlExtensions.cs
@@ -54,4 +54,56 @@ public static class ListControlExtensions
 		listControl.DataBindings.Add(nameof(listControl.SelectedValue), dataSource, dataMember, true, DataSourceUpdateMode.OnPropertyChanged);
 		return listControl;
 	}
+
+	/// <summary>
+	/// Sets the <see cref="ListControl.DisplayMember"/> property of the specified <see cref="ListControl"/>.
+	/// </summary>
+	/// <param name="listControl">The <see cref="ListControl"/> to set the display member for.</param>
+	/// <param name="displayMember">The name of the property to display for each item in the list.</param>
+	/// <returns>
+	/// The <see cref="ListControl"/> control with the display member set, allowing for method chaining.
+	/// </returns>
+	public static ListControl WithDisplayMember(this ListControl listControl, string displayMember)
+	{
+		listControl.DisplayMember = displayMember;
+		return listControl;
+	}
+
+	/// <summary>
+	/// Sets the <see cref="ListControl.ValueMember"/> property of the specified <see cref="ListControl"/>.
+	/// </summary>
+	/// <param name="listControl">The <see cref="ListControl"/> to set the value member for.</param>
+	/// <param name="valueMember">The name of the property to use as the actual value for each item in the list.</param>
+	/// <returns>
+	/// The <see cref="ListControl"/> control with the value member set, allowing for method chaining.
+	/// </returns>
+	public static ListControl WithValueMember(this ListControl listControl, string valueMember)
+	{
+		listControl.ValueMember = valueMember;
+		return listControl;
+	}
+
+	/// <summary>
+	/// Binds the <see cref="ListControl.DataSource"/> property of the specified <see cref="ListControl"/>
+	/// to an enumeration of type <typeparamref name="T"/>.
+	/// </summary>
+	/// <typeparam name="T">The enumeration type to bind to the <see cref="ListControl.DataSource"/>.</typeparam>
+	/// <param name="listControl">The <see cref="ListControl"/> to bind.</param>
+	/// <param name="value">The enumeration value used to determine the type <typeparamref name="T"/>.</param>
+	/// <returns>
+	/// The <see cref="ListControl"/> control with the binding applied, allowing for method chaining.
+	/// </returns>
+	public static ListControl WithEnumeratorBinding<T>(this ListControl listControl, T value)
+		where T : struct, IComparable, IFormattable, IConvertible
+	{
+		List<KeyValuePair<T, string>> datasource = [.. Enum.GetValues(value.GetType())
+			.OfType<T>()
+			.ToDictionary(key => key, value => $"{value}")];
+
+		listControl.WithDisplayMember(nameof(KeyValuePair<T, string>.Value))
+			.WithValueMember(nameof(KeyValuePair<T, string>.Key))
+			.WithDataSourceBinding(datasource);
+
+		return listControl;
+	}
 }
