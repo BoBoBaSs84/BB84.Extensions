a6b2689dc2a6a3cc43494e899c1558ef93e3470b
Probotus <137200367+Probotus@users.noreply.github.com>
2025-10-03 22:56:48 +0200

    feat: byte default check methods added

---
+++ tests/BB84.Extensions.Tests/ByteExtensionsTests.IsDefault.cs
@@ -0,0 +1,34 @@
+﻿// Copyright: 2023 Robert Peter Meyer
+// License: MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+namespace BB84.Extensions.Tests;
+
+public sealed partial class ByteExtensionsTests
+{
+	[TestMethod]
+	[Description("Should determine whether a byte is its default value (0).")]
+	public void IsDefaultLong()
+	{
+		byte value = default;
+		Assert.IsTrue(value.IsDefault());
+
+		value = 15;
+		Assert.IsFalse(value.IsDefault());
+	}
+
+	[TestMethod]
+	[Description("Should determine whether a nullable byte is its default value (null).")]
+	public void IsDefaultNullableLong()
+	{
+		byte? value = default;
+		Assert.IsTrue(value.IsDefault());
+
+		value = null;
+		Assert.IsTrue(value.IsDefault());
+
+		value = 15;
+		Assert.IsFalse(value.IsDefault());
+	}
+}
