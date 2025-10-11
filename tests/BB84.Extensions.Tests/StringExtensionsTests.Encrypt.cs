78ecad7b1d0f570a40f1720a572d52b8f9957569
Probotus <137200367+Probotus@users.noreply.github.com>
2025-10-11 18:13:41 +0200

    feat: unit tests for string methods added

---
+++ tests/BB84.Extensions.Tests/StringExtensionsTests.Encrypt.cs
@@ -0,0 +1,23 @@
+﻿// Copyright: 2023 Robert Peter Meyer
+// License: MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+namespace BB84.Extensions.Tests;
+
+public sealed partial class StringExtensionsTests
+{
+	[TestMethod]
+	public void EncryptDecryptSuccessTest()
+	{
+		string original = "This is a test string for encryption.";
+		string password = "StrongPassword123!";
+		string encrypted = original.Encrypt(password);
+
+		Assert.AreNotEqual(original, encrypted);
+
+		string decrypted = encrypted.Decrypt(password);
+
+		Assert.AreEqual(original, decrypted);
+	}
+}
---
+++ tests/BB84.Extensions.Tests/StringExtensionsTests.Join.cs
@@ -0,0 +1,14 @@
+﻿// Copyright: 2023 Robert Peter Meyer
+// License: MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+namespace BB84.Extensions.Tests;
+
+public sealed partial class StringExtensionsTests
+{
+	[TestMethod]
+	[DynamicData(nameof(GetJoinTestData), DynamicDataSourceType.Method)]
+	public void JoinTest(string[] values, string separator, string expected)
+		=> Assert.AreEqual(expected, values.Join(separator));
+}
