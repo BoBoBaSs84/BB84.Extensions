2e4f4065f12d2dc0abaca09fca4397bd514bc66f
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2025-11-03 18:30:38 +0100

    feat: additional unit tests added

--- tests/BB84.Extensions.Tests/EnumerableExtensionsTests.cs
+++ tests/BB84.Extensions.Tests/EnumerableExtensionsTests.cs
@@ -8,6 +8,106 @@ namespace BB84.Extensions.Tests;
 [TestClass]
 public sealed partial class EnumerableExtensionsTests
 {
+	[TestMethod]
+	public void IsEmptyWithEmptyCollectionReturnsTrue()
+	{
+		IEnumerable<int> collection = [];
+
+		bool result = collection.IsEmpty();
+
+		Assert.IsTrue(result);
+	}
+
+	[TestMethod]
+	public void IsEmptyWithNonEmptyCollectionReturnsFalse()
+	{
+		IEnumerable<int> collection = [1, 2, 3];
+
+		bool result = collection.IsEmpty();
+
+		Assert.IsFalse(result);
+	}
+
+	[TestMethod]
+	public void IsNotEmptyWithEmptyCollectionReturnsFalse()
+	{
+		IEnumerable<int> collection = [];
+
+		bool result = collection.IsNotEmpty();
+
+		Assert.IsFalse(result);
+	}
+
+	[TestMethod]
+	public void IsNotEmptyWithNonEmptyCollectionReturnsTrue()
+	{
+		IEnumerable<int> collection = [1, 2, 3];
+
+		bool result = collection.IsNotEmpty();
+
+		Assert.IsTrue(result);
+	}
+
+	[TestMethod]
+	public void IsNullOrEmptyWithNullCollectionReturnsTrue()
+	{
+		IEnumerable<int>? collection = null;
+
+		bool result = collection.IsNullOrEmpty();
+
+		Assert.IsTrue(result);
+	}
+
+	[TestMethod]
+	public void IsNullOrEmptyWithEmptyCollectionReturnsTrue()
+	{
+		IEnumerable<int>? collection = [];
+
+		bool result = collection.IsNullOrEmpty();
+
+		Assert.IsTrue(result);
+	}
+
+	[TestMethod]
+	public void IsNullOrEmptyWithNonEmptyCollectionReturnsFalse()
+	{
+		IEnumerable<int>? collection = [1, 2, 3];
+		
+		bool result = collection.IsNullOrEmpty();
+		
+		Assert.IsFalse(result);
+	}
+
+	[TestMethod]
+	public void IsNotNullOrEmptyWithNullCollectionReturnsFalse()
+	{
+		IEnumerable<int>? collection = null;
+		
+		bool result = collection.IsNotNullOrEmpty();
+		
+		Assert.IsFalse(result);
+	}
+
+	[TestMethod]
+	public void IsNotNullOrEmptyWithEmptyCollectionReturnsFalse()
+	{
+		IEnumerable<int>? collection = [];
+		
+		bool result = collection.IsNotNullOrEmpty();
+		
+		Assert.IsFalse(result);
+	}
+
+	[TestMethod]
+	public void IsNotNullOrEmptyWithNonEmptyCollectionReturnsTrue()
+	{
+		IEnumerable<int>? collection = [1, 2, 3];
+		
+		bool result = collection.IsNotNullOrEmpty();
+		
+		Assert.IsTrue(result);
+	}
+
 	private sealed class TestClass
 	{
 		public int Value { get; set; }
