96918b10416c99a8c0a827827378a6508b850f48
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2025-11-21 17:43:10 +0100

    fix: ms test settings added

---
+++ tests/BB84.WinForms.Extensions.Tests/MsTestSettings.cs
@@ -0,0 +1,6 @@
+﻿// Copyright: 2023 Robert Peter Meyer
+// License: MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
