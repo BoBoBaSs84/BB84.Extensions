cf6681844b43e20639aab585087c670234a45e29
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2026-06-10 17:40:47 +0200

    feat(winforms): add `NumericUpDown` binding extensions for all key properties
    - Extended `NumericUpDownExtensions` with `WithMinimumBinding`, `WithMaximumBinding`, `WithIncrementBinding`, and `WithDecimalPlacesBinding` methods.
    - Added unit tests for all new binding methods.
    - Updated documentation and `README` with usage examples.

--- src/BB84.WinForms.Extensions/NumericUpDownExtensions.cs
+++ src/BB84.WinForms.Extensions/NumericUpDownExtensions.cs
@@ -7,7 +7,9 @@ namespace BB84.WinForms.Extensions;
 
 /// <summary>
 /// Provides extension methods for binding properties of a <see cref="NumericUpDown"/> control to a data source,
-/// enabling streamlined data binding for common properties such as <see cref="NumericUpDown.Value"/>.
+/// enabling streamlined data binding for common properties such as <see cref="NumericUpDown.Value"/>,
+/// <see cref="NumericUpDown.Minimum"/>, <see cref="NumericUpDown.Maximum"/>,
+/// <see cref="NumericUpDown.Increment"/>, and <see cref="NumericUpDown.DecimalPlaces"/>.
 /// </summary>
 public static class NumericUpDownExtensions
 {
@@ -28,6 +30,86 @@ public static class NumericUpDownExtensions
 	public static NumericUpDown WithValueBinding(this NumericUpDown numericUpDown, object dataSource, string dataMember)
 	{
 		numericUpDown.DataBindings.Add(nameof(numericUpDown.Value), dataSource, dataMember, true, DataSourceUpdateMode.OnPropertyChanged);
+		return numericUpDown;
+	}
+
+	/// <summary>
+	/// Binds the <see cref="NumericUpDown.Minimum"/> property of the specified <see cref="NumericUpDown"/>
+	/// to a property on the provided data source.
+	/// </summary>
+	/// <remarks>
+	/// The binding is configured to update the data source whenever the <see cref="NumericUpDown.Minimum"/> 
+	/// property changes, using <see cref="DataSourceUpdateMode.OnPropertyChanged"/>.
+	/// </remarks>
+	/// <param name="numericUpDown">The <see cref="NumericUpDown"/> to bind.</param>
+	/// <param name="dataSource">The data source containing the property to bind to.</param>
+	/// <param name="dataMember">The name of the property on the data source to bind to.</param>
+	/// <returns>
+	/// The <see cref="NumericUpDown"/> control with the binding applied, allowing for method chaining.
+	/// </returns>
+	public static NumericUpDown WithMinimumBinding(this NumericUpDown numericUpDown, object dataSource, string dataMember)
+	{
+		numericUpDown.DataBindings.Add(nameof(numericUpDown.Minimum), dataSource, dataMember, true, DataSourceUpdateMode.OnPropertyChanged);
+		return numericUpDown;
+	}
+
+	/// <summary>
+	/// Binds the <see cref="NumericUpDown.Maximum"/> property of the specified <see cref="NumericUpDown"/>
+	/// to a property on the provided data source.
+	/// </summary>
+	/// <remarks>
+	/// The binding is configured to update the data source whenever the <see cref="NumericUpDown.Maximum"/> 
+	/// property changes, using <see cref="DataSourceUpdateMode.OnPropertyChanged"/>.
+	/// </remarks>
+	/// <param name="numericUpDown">The <see cref="NumericUpDown"/> to bind.</param>
+	/// <param name="dataSource">The data source containing the property to bind to.</param>
+	/// <param name="dataMember">The name of the property on the data source to bind to.</param>
+	/// <returns>
+	/// The <see cref="NumericUpDown"/> control with the binding applied, allowing for method chaining.
+	/// </returns>
+	public static NumericUpDown WithMaximumBinding(this NumericUpDown numericUpDown, object dataSource, string dataMember)
+	{
+		numericUpDown.DataBindings.Add(nameof(numericUpDown.Maximum), dataSource, dataMember, true, DataSourceUpdateMode.OnPropertyChanged);
+		return numericUpDown;
+	}
+
+	/// <summary>
+	/// Binds the <see cref="NumericUpDown.Increment"/> property of the specified <see cref="NumericUpDown"/>
+	/// to a property on the provided data source.
+	/// </summary>
+	/// <remarks>
+	/// The binding is configured to update the data source whenever the <see cref="NumericUpDown.Increment"/> 
+	/// property changes, using <see cref="DataSourceUpdateMode.OnPropertyChanged"/>.
+	/// </remarks>
+	/// <param name="numericUpDown">The <see cref="NumericUpDown"/> to bind.</param>
+	/// <param name="dataSource">The data source containing the property to bind to.</param>
+	/// <param name="dataMember">The name of the property on the data source to bind to.</param>
+	/// <returns>
+	/// The <see cref="NumericUpDown"/> control with the binding applied, allowing for method chaining.
+	/// </returns>
+	public static NumericUpDown WithIncrementBinding(this NumericUpDown numericUpDown, object dataSource, string dataMember)
+	{
+		numericUpDown.DataBindings.Add(nameof(numericUpDown.Increment), dataSource, dataMember, true, DataSourceUpdateMode.OnPropertyChanged);
+		return numericUpDown;
+	}
+
+	/// <summary>
+	/// Binds the <see cref="NumericUpDown.DecimalPlaces"/> property of the specified <see cref="NumericUpDown"/>
+	/// to a property on the provided data source.
+	/// </summary>
+	/// <remarks>
+	/// The binding is configured to update the data source whenever the <see cref="NumericUpDown.DecimalPlaces"/> 
+	/// property changes, using <see cref="DataSourceUpdateMode.OnPropertyChanged"/>.
+	/// </remarks>
+	/// <param name="numericUpDown">The <see cref="NumericUpDown"/> to bind.</param>
+	/// <param name="dataSource">The data source containing the property to bind to.</param>
+	/// <param name="dataMember">The name of the property on the data source to bind to.</param>
+	/// <returns>
+	/// The <see cref="NumericUpDown"/> control with the binding applied, allowing for method chaining.
+	/// </returns>
+	public static NumericUpDown WithDecimalPlacesBinding(this NumericUpDown numericUpDown, object dataSource, string dataMember)
+	{
+		numericUpDown.DataBindings.Add(nameof(numericUpDown.DecimalPlaces), dataSource, dataMember, true, DataSourceUpdateMode.OnPropertyChanged);
 		return numericUpDown;
 	}
 }
